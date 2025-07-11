c73333477184b6d0c880b6963c1f3918009c19b5
Михаил Дорошин <demargorn@gmail.com>
2025-07-11 13:03:02 +0300

    save popup fixed

--- src/shared/Popups/SavePopup.tsx
+++ src/shared/Popups/SavePopup.tsx
@@ -13,9 +13,9 @@ const SavePopup = ({ active, setActive }: TypeSavePopupProps) => {
 
   return (
     <div className={active ? cn(styles.container, styles.active) : `${styles.container}`} onClick={() => setActive(false)}>
-      <div className={active ? cn(styles.content, styles.active) : `${styles.content}`} onClick={(e) => e.stopPropagation()}>
-        <h3 className={styles.heading}>Сохранено</h3>
-        <button className={styles.btn_cross} onClick={() => setActive(false)}></button>
+      <div className={active ? cn(styles.content, styles.content_save, styles.active) : cn(styles.content, styles.content_save)} onClick={(e) => e.stopPropagation()}>
+        <h3 className={cn(styles.heading, styles.heading_save)}>Сохранено</h3>
+        {/* <button className={styles.btn_cross} onClick={() => setActive(false)}></button> */}
         <div className={styles.btn_container_save}>
           <button className={cn(styles.btn_cancel, styles.btn_save)} onClick={() => setActive(false)}>
             Назад
